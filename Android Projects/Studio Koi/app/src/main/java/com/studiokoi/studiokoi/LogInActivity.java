f21873679b7b0a3ac3c808fb27fb4088377c80cb
Debasishroy <roydebasish0010@gmail.com>
2020-07-07 08:14:30 +0600

    New project and Log In Page Added

---
+++ Android Projects/Studio Koi/app/src/main/java/com/studiokoi/studiokoi/LogInActivity.java
@@ -0,0 +1,29 @@
+package com.studiokoi.studiokoi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LogInActivity extends AppCompatActivity {
+
+    private TextView singup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+
+        singup=findViewById(R.id.sign_up);
+
+        singup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(getApplicationContext(),SignUpActivity.class));
+                finish();
+            }
+        });
+    }
+}
